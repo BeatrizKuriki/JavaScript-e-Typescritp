d397a46964581d784abfbb439b4a3c496e0e63d4
Beatriz Alencar Kuriki <b.f.kuriki@gmail.com>
2022-12-04 19:12:20 -0300

    comentários e console.log

---
+++ JS/aulas/aula3/index.js
@@ -0,0 +1,6 @@
+console.log('Beatriz Alencar');
+const nome = 'Beatriz ALencar';
+console.log(nome);
+
+let nome1 = 'Stella Kuriki';
+console.log('Olá meu nome é ' + nome1+'.Estou aprendendo Javascript as 10 da manhã.')
---
+++ JS/aulas/aula4/comentarios.js
@@ -0,0 +1,8 @@
+//o comentário poder ser feito com duas barras e não será lido pela engine
+//do JS
+
+console.log(25);
+/* 
+ncflkalfkjsfkljg
+*/
+
