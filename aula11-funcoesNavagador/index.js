4aa9e06a85198228aa78f9925d83661f5bfc2c0a
Beatriz Alencar Kuriki <b.f.kuriki@gmail.com>
2022-12-07 11:50:43 -0300

    exercício utilizando prompt, alert e confirm

---
+++ aula11-funcoesNavagador/index.js
@@ -0,0 +1,15 @@
+/* FUNÇÕES DO NAVEGADOR: ALERT/ PROMPT/ CONFIRM */
+
+alert('Nossa mensagem!!');
+
+
+
+prompt('Digite seu nome:');
+
+let confirma = confirm('Deseja realmento apagar?');
+
+let num1 = Number(prompt('Digite um número:'));
+let num2 = Number(prompt('Digite um número:'));
+
+let soma = num1+num2;
+alert(soma);
