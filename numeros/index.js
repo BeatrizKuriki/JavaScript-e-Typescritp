7e95a2b0491c4073d318f652141505b0ff9eaa79
Beatriz Alencar Kuriki <b.f.kuriki@gmail.com>
2022-12-08 12:07:01 -0300

    números no js

---
+++ numeros/index.js
@@ -0,0 +1,13 @@
+//IEEE 754-2008 padrão do js para precisão de números
+
+let num1 = 1500.32;
+let num2 = 2.5;
+
+console.log(num1.toString()+num2);
+console.log(num1.toString(2));
+num1.toFixed(1) //arredondar numeros e definir as casas decimais.
+
+let n1=0.7;
+let n2=0.1;
+console.log(n1+n2);
+
