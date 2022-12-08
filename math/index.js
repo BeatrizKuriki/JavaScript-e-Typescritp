6adaf809c4882067893265a2883ba7e1dc99ec27
Beatriz Alencar Kuriki <b.f.kuriki@gmail.com>
2022-12-08 12:38:01 -0300

    objeto Math em js

---
+++ math/index.js
@@ -0,0 +1,21 @@
+let num1 = 9.54578;
+let num2 = Math.floor(num1);
+let num3 = Math.ceil(num1);
+
+console.log(num2);
+console.log(num3);
+
+let n1 = 9.1546;
+let n2 = Math.round(n1);
+console.log('n2:'+n2);
+
+console.log(Math.max(1,2,-10,15,100,98,-25));
+console.log(Math.min(1,2,-10,15,100,98,-25));
+console.log(Math.random());
+
+const aleatorio = Math.round(Math.random() * (10 -5) +5);//aqui ele faz um número aleatório
+//entre 10 e 5, pode ser arredondado usando math.round
+console.log(aleatorio);
+
+console.log(Math.pow(2,2));
+console.log(2**3);
