c680ad1708678c61d6b4ba1f5e6d25f17f3acd89
Beatriz Alencar Kuriki <b.f.kuriki@gmail.com>
2023-01-11 13:46:37 -0300

    finalizado

--- exerciciosArraysObjetosFunctions/js/script.js
+++ exerciciosArraysObjetosFunctions/js/script.js
@@ -13,6 +13,9 @@
 
 function meuEscopo(){
     const form = document.querySelector('.form');
+    const resultado = document.querySelector('.resultado');
+
+    const pessoas = [];
       
     function recebeEventoForm (evento){
         evento.preventDefault();
@@ -20,7 +23,20 @@ function meuEscopo(){
         const sobrenome = form.querySelector('.sobrenome');
         const peso = form.querySelector('.peso');
         const altura = form.querySelector('.altura');
-        console.log(nome.value, sobrenome.value, peso.value, altura.value) 
+
+        pessoas.push({
+            nome: nome.value,
+            sobrenome: sobrenome.value,
+            peso: peso.value,
+            altura: altura.value
+
+        });
+
+        console.log(pessoas);
+
+        resultado.innerHTML += `<p> ${nome.value} ${sobrenome.value} `+
+        `${peso.value} ${altura.value}</p>`
+       
     }    
     form.addEventListener('submit', recebeEventoForm)
 
